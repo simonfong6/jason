e8e5c4fd05fd1a3f374d7765c487ada16c54dc1c
Jason <jasonyoo92@gmail.com>
2020-09-11 19:23:08 -0700

    set up file

---
+++ backend/backend/server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Backend server.
+"""
+import time
+
+from flask import Flask
+from flask import send_from_directory
+
+
+
+
+app = Flask(
+    __name__
+)
+
+
+
+@app.route('/time')
+def get_current_time():
+    return {
+        'time': time.time(),
+        'status': 'failure',
+        'version': 0.6
+    }
+
+
+# app.run(debug = True)
